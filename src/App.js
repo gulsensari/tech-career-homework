b88d4ded753530c3b178cf6a94ace903239a571d
gulsensari <52375039+gulsensari@users.noreply.github.com>
2022-05-12 15:50:47 +0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import Navbar from "./template/Navbar";
+import PageContent from "./template/PageContent";
+import Slide from "./template/Slide";
+import Footer from "./template/Footer";
+
+function App() {
+  return (<>
+  <Navbar></Navbar>
+  <Slide></Slide>
+  <PageContent></PageContent>
+  <Footer></Footer>
+  </>
+ );
+}
+
+export default App;
